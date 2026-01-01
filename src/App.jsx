fd2f9fa090aa6ba48cc5a9352f8bb02318773c47
Suryansh Dixit <150290655+Blissysurya@users.noreply.github.com>
2026-01-02 02:23:31 +0530

    as

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import {RiPencilLine} from '@remixicon/react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='h-screen w-full overflow-hidden bg-black'>
+        <div id='input-div' className='flex  mt-4'>
+          <textarea className='m-auto h-96 w-10/11 bg-white rounded-2xl focus:outline-none focus:ring-0 resize-none p-10' type='text' id='input-box' placeholder='Enter your task here' />
+          <RiPencilLine className='bg-green-600 absolute mt-80 mr-10' />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
